f9fc7e81a497d8879bb4128669703c3d99d97fee
jennkalidoss <jennkalidoss@gmail.com>
2018-05-05 16:22:32 -0700

    HTML draft & file structure

---
+++ imageTrust/app/js/app.js
@@ -0,0 +1,7 @@
+function app()	{
+	if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
+  web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
+  console.log("Using web3 version: " + Web3.version);
+}
+
+$(document).ready(app);  
