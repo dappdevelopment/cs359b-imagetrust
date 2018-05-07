cf4367ba8643108fa2bae7cfd5e91d6095f70509
khegazy <kareemhegz@gmail.com>
2018-05-06 23:12:06 -0700

    Edit constructor

--- imageTrust/contracts/validation.sol
+++ imageTrust/contracts/validation.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.4.21;
 
 contract code_validation {
   address creator = address(0);
+  string company_name = "None"
+  mapping (string => bytes32) code_hashes; 
 
-
-  function code_validation() public {
+  function code_validation(string _company_name) public {
     creator = msg.sender;
+    company_name = _company_name;
   }
 
 }
