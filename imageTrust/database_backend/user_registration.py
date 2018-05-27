9ee264252dcdc73124e2bb8bee4624d85c71fdd2
khegazy <kareemhegz@gmail.com>
2018-05-27 16:00:52 -0700

    Add user information to dictionary to check against for required fields and to add to MySQL database.

--- imageTrust/database_backend/user_registration.py
+++ imageTrust/database_backend/user_registration.py
@@ -2,6 +2,7 @@ import httplib
 import ssl
 import OpenSSL
 import MySQLdb
+import collections
 
 
 
@@ -15,7 +16,11 @@ dataBaseUserName  = "imageTrust"
 dataBasePassword  = "superSecret"
 dataBaseName      = "userInformation"
 
+# variables for debugging
 userName = "testUser"
+userFirstName = "Jimmy-John-Jones"
+userKeyLink = "www.google.com"
+userPassword = "superdupersecret"
 
 ###############################################
 #####  Get user information from website  #####
@@ -30,9 +35,38 @@ if response.status != 200:
   raise RuntimeError("userKeyLinkDown")
 
 ###  Get certificate  ###
-cert = ssl.get_server_certificate(('www.google.com', 443))
+cert = ssl.get_server_certificate((userKeyLink, 443))
 x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
 certInfo = x509.get_subject().get_components()
 
 
+
+#########################################
+#####  Add information to database  #####
+#########################################
+
+###  Gather information  ###
+userInformation = collections.defaultdict(None)
+
+userInformation["UserName"] = userName
+userInformation["FirstName"] = userFirstName
+userInformation["Password"] = userPassword
+for key,val in certInfo:
+  if key is 'C':
+    userInformation["Country"] = val
+  elif key is 'ST':
+    userInformation["State"] = val
+  elif key is 'L':
+    userInformation["City"] = val
+  elif key is 'O':
+    userInformation["Company"] = val
+  elif key is 'CN':
+    userInformation["CommonName"] = val
+
+# TODO read in txt file and get key
+userInformation["PublicKey"] = "leKey"
+
+for key in userInformation:
+  if userInformation[key] is None:
+    raise RuntimeError("Requre-" + key)
 
