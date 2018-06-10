7f0c13799421cd13401b56e0f9d7608a96625504
khegazy <kareemhegz@gmail.com>
2018-06-09 19:12:42 -0700

    Added ownable functionality and allows imageTrust to add new users.

---
+++ imageTrust/contracts/ownable.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.23;
+
+contract Ownable {
+
+  address public owner;
+  address public imageTrust=address(0xE4Fe1a25BA4aab866F00285088D20E39Cd2c9d3e);
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+}
